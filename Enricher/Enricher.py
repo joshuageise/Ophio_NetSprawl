5ae46d105d391d1e52f9966e913c6f7e3ba6e14e
Liam Decker <liam.decker@protonmail.com>
2018-10-13 16:06:32 -0400

    cleaned up, stubbed out enricher

---
+++ Enricher/Enricher.py
@@ -0,0 +1,5 @@
+
+def scanHostForInfo(ipAddress:str):
+    pass
+    #Something like define and interface for scanning ports/versions
+    #Also an interface for scanning operating system.
--- Identifier/identifier.py
+++ Identifier/identifier.py
@@ -1,21 +1,6 @@
-import subprocess
-import re
 import json
 import hostdiscovery
 import networkinfo
-
-"""
-This regex looks like complete gibbrish, but when run on the "ip a" command should produce the following output:
-Group 1: interface name
-Group 2: interface MAC address
-Group 3: Interface IP address in CIDR notation
-Group 4: Interface Broadcast address
-
-This may return multiple matches, as there are multiple interfaces.
-"""
-def ip_a_regex():
-    return r"\d: (.*):.*\n\s*link.* (.*) brd.*\n\s*inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+) " \
-           "(?:brd)? ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)"
 
 def scanCurrentNetwork():
     interfaces = []
