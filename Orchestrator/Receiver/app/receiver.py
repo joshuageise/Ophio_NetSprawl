5237090a334fb982136480019e71e55c7e00dcda
Jacob Ryder <jcr5310@rit.edu>
2018-10-16 20:18:51 -0400

    Basic web app to receive json from agents.

---
+++ Orchestrator/Receiver/app/receiver.py
@@ -0,0 +1,28 @@
+# Basic Flask app, bit hello world and bit PoC
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return "Test endpoint."
+
+@app.route('/receive', methods=['GET', 'POST'])
+def receive():
+
+    if request.content_length == 0 or not request.is_json:
+        # return a 400 code
+        return "json required", 400
+
+    else:
+        # parse json sent, plus other interesting info
+        json = request.json
+        addr = request.remote_addr
+
+        # forward it to the database? call or return to the enricher?
+        # drop it as a file for now
+        with open("Reports/{}".format(addr), "w") as f:
+            f.write(str(json))
+
+        # return a 200
+        return "received", 200
