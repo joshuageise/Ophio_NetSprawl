041296cc853759e5c64bf98f5d508706f9ea921d
Jacob Ryder <jcr5310@rit.edu>
2018-11-13 10:56:34 -0500

    Host record formatting, init Identifier in lifecycle.

--- src/lifecycle.py
+++ src/lifecycle.py
@@ -2,40 +2,82 @@ import Identifier
 import Enricher
 import Orchestrator
 import Exploiter
+from record import Record
+
+import pymongo
+from metasploit.msfrpc import MsfRpcClient
 
 def main():
-    print("hello world")
-
-    # setup
-    # check for dependencies
+    ### setup
+    # check for dependencies? - nmap, ms, mongo
     # check for legally scanned network?
-    # copy + zip identifier for post-exploits
+    # copy + zip identifier for post-exploits?
+    # initialize pointers for orchestration
     # connect to database
     # initialize exploits + data for selector
     # initialize logging
 
-    # identifier
+    hostCollection = [] # want to define a full host class
+    ipAddrsSeen= [] # dedicated parallel list for previously seen IPs
+    rootHost = None
+
+    enrichQueue = []
+    exploitQueue = []
+    postexQueue = []
+
+    ### identifier
     # call scanCurrentNetwork on initial host
     # log results of scan
-    # add host info to database
+    # identify root node
+    # add host records to database
+    netScanResults = Identifier.scanCurrentNetwork()
+    rootInterfaces = []
+    hostsDiscovered = []
+    for network in netScanResults:
+        rootInterfaces.append(network["network"])
+        for hostIp in network["hosts"]:
+            if hostIp not in rootInterfaces:
+                hostsDiscovered.append(hostIp)
 
-    # enricher
+    rootHost = Record(rootInterfaces, None)
+    # TODO commit rootHost to db, generate id
+    rootHost.exploitStatus["statusCode"] = Record.STATUS_SUCCESS
+    rootHost.exploitStatus["exploitUsed"] = "N/A"
+    hostCollection.append(rootHost)
+    enrichQueue.append(rootHost)
+    for hostIp in hostsDiscovered:
+        hostRecord = Record([hostIp], rootHost.id)
+        # TODO commit hostRecord to db, generate id
+        hostCollection.append(hostRecord)
+        enrichQueue.append(hostRecord)
+
+    ### enricher
     # call scanHostsForInfo on list of hosts, excluding self
     # log results of scans
     # append information to host records in database
+    while len(enrichQueue) > 0:
+        host = enrichQueue.pop()
+        enrichResults = Enricher.scanHostForInfo(host)
+        #update host obj
+        exploitQueue.append(host)
 
-    # selector/exploiter
+
+
+    ### selector/exploiter
     # call search on enriched host data
     # attempt exploits in recommended order
     # log results and update selector after each exploit run
     # append exploit status (exploit used, MS session) to host records
+    while len(exploitQueue) > 0:
+        host = exploitQueue.pop()
 
-    # post-exploits
+
+    ### post-exploits
     # drop and run identifier on exploited boxes
     # add new hosts to records + database
     # enrich and exploit new hosts as usual
 
-    # repetition
+    ### repetition
     # each host should be identified, enriched, exploited, and scanned from:
     # a) seperate queues for each stage, evaluated independently
     # b) single queue, each host evaluated straight through
---
+++ src/record.py
@@ -0,0 +1,29 @@
+class Record:
+    STATUS_UNEXPLORED = 0
+    STATUS_SUCCESS = 1
+    STATUS_FAILURE = 2
+
+    """ Object to represent a discovered host throughout its lifecycle. """
+    def __init__(self, interfaces, parentId):
+        self.id = None
+        self.interfaces = interfaces
+        self.parentId = parentId
+        self.os = None
+        self.openPorts = None
+        self.exploitStatus = {
+            "statusCode": STATUS_UNEXPLORED,
+            "exploitUsed": None,
+            "msSessionId": None
+        }
+
+    def toDict(self):
+        dict = {
+            "interfaces": self.interfaces,
+            "parentId": self.parentId,
+            "os": self.os,
+            "openPorts": self.openPorts,
+            "exploitStatus": self.exploitStatus
+        }
+        if self.id != None:
+            dict["_id"] = self.id
+        return dict
