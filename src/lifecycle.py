d27c663c179b6eee56ba44d649a6c0ebc9275c3b
Jacob Ryder <jcr5310@rit.edu>
2018-11-13 14:02:28 -0500

    Enricher in lifecycle.

--- src/lifecycle.py
+++ src/lifecycle.py
@@ -17,48 +17,58 @@ def main():
     # initialize exploits + data for selector
     # initialize logging
 
-    hostCollection = [] # want to define a full host class
-    ipAddrsSeen= [] # dedicated parallel list for previously seen IPs
+    hostCollection = []
     rootHost = None
 
     enrichQueue = []
     exploitQueue = []
     postexQueue = []
 
+    # TODO connect to mango
+
+    # strategy = ...
+
+
+
     ### identifier
     # call scanCurrentNetwork on initial host
     # log results of scan
     # identify root node
     # add host records to database
-    netScanResults = Identifier.scanCurrentNetwork()
+
+    identifyResults = Identifier.scanCurrentNetwork()
     rootInterfaces = []
     hostsDiscovered = []
-    for network in netScanResults:
+    for network in identifyResults:
         rootInterfaces.append(network["network"])
         for hostIp in network["hosts"]:
             if hostIp not in rootInterfaces:
                 hostsDiscovered.append(hostIp)
 
     rootHost = Record(rootInterfaces, None)
-    # TODO commit rootHost to db, generate id
+    # TODO insert rootHost.toDict() to db, generate + store id
     rootHost.exploitStatus["statusCode"] = Record.STATUS_SUCCESS
     rootHost.exploitStatus["exploitUsed"] = "N/A"
     hostCollection.append(rootHost)
     enrichQueue.append(rootHost)
     for hostIp in hostsDiscovered:
         hostRecord = Record([hostIp], rootHost.id)
-        # TODO commit hostRecord to db, generate id
+        # TODO insert hostRecord.toDict() to db, generate + store id
         hostCollection.append(hostRecord)
         enrichQueue.append(hostRecord)
+
 
     ### enricher
     # call scanHostsForInfo on list of hosts, excluding self
     # log results of scans
     # append information to host records in database
+
     while len(enrichQueue) > 0:
-        host = enrichQueue.pop()
-        enrichResults = Enricher.scanHostForInfo(host)
-        #update host obj
+        hostRecord = enrichQueue.pop()
+        enrichResults = Enricher.scanHostForInfo(hostRecord.interfaces)
+        hostRecord.os = enrichResults[0]
+        hostRecord.openPorts = enrichResults[1:]
+        # TODO update hostRecord, filtering by hostRecord.id
         exploitQueue.append(host)
 
 
@@ -68,14 +78,19 @@ def main():
     # attempt exploits in recommended order
     # log results and update selector after each exploit run
     # append exploit status (exploit used, MS session) to host records
+
     while len(exploitQueue) > 0:
-        host = exploitQueue.pop()
+        hostRecord = exploitQueue.pop()
+
 
 
     ### post-exploits
     # drop and run identifier on exploited boxes
     # add new hosts to records + database
     # enrich and exploit new hosts as usual
+
+
+
 
     ### repetition
     # each host should be identified, enriched, exploited, and scanned from:
